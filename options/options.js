0a53c848fe0141794c6464cc4e1f57c90dad4079
Crowdbotics <team@crowdbotics.com>
2023-01-25 17:44:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lucky-shadow-38459",
+  url: "https://lucky-shadow-38459.botics.co",
+  api: "https://lucky-shadow-38459.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
